6270afae862b4f3f660311955f9b95e62fb00926
Kirk <kmadera@robofirm.com>
2018-09-10 12:17:57 -0500

    Fixed snapshot excludes naming convention

--- config/platform.php
+++ config/platform.php
@@ -1,33 +1,35 @@
 <?php
 
 return [
-    'asset_groups' => [
-        'core'           => [
-            '/css',
-            '/config_*',
-            '/js',
-            '/php/twig',
+    'snapshot'      => [
+        'asset_groups'          => [
+            'core'           => [
+                '/css',
+                '/config_*',
+                '/js',
+                '/php/twig',
+            ],
+            'common_modules' => [],
         ],
-        'common_modules' => [],
-    ],
-    'database_table_groups' => [
-        'core'           => [
-            'cache_bootstrap',
-            'cache_config',
-            'cache_container',
-            'cache_data',
-            'cache_default',
-            'cache_discovery',
-            'cache_dynamic_page_cache',
-            'cache_entity',
-            'cache_library',
-            'cache_menu',
-            'cache_render',
-            'cache_toolbar',
+        'database_table_groups' => [
+            'core'           => [
+                'cache_bootstrap',
+                'cache_config',
+                'cache_container',
+                'cache_data',
+                'cache_default',
+                'cache_discovery',
+                'cache_dynamic_page_cache',
+                'cache_entity',
+                'cache_library',
+                'cache_menu',
+                'cache_render',
+                'cache_toolbar',
+            ],
+            'common_modules' => [],
         ],
-        'common_modules' => [],
     ],
-    'template_vars'         => [
+    'template_vars' => [
         'database_host'         => 'localhost',
         'database_port'         => 3306,
         'database_table_prefix' => '',
